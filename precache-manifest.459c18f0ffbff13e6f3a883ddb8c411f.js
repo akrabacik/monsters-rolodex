4c2003c43da412f8c693ba62005841b36135b102
akrabacik <kuzeymertcetin@hotmail.com>
2020-10-31 22:45:04 +0300

    Updates

---
+++ precache-manifest.459c18f0ffbff13e6f3a883ddb8c411f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b1b4ab0cd96352ccefcdae584b27b8d",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "e9aaddbf85ee0cbf8e89",
+    "url": "/monsters-rolodex/static/css/main.ff9cb60f.chunk.css"
+  },
+  {
+    "revision": "88b685495fee72b0273c",
+    "url": "/monsters-rolodex/static/js/2.2eaf0afe.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/monsters-rolodex/static/js/2.2eaf0afe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9aaddbf85ee0cbf8e89",
+    "url": "/monsters-rolodex/static/js/main.b2012a8b.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
